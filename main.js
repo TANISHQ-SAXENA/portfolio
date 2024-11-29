d00e2abae7d15735e74e39cec8da1c06bb4d7885
Tanishq Saxena <saxenatanishq22@gmail.com>
2024-11-29 13:43:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+        var typed = new Typed('#element', {
+            strings: ['Web Developer.', 'Frontend Developer.', 'Backend Developer.', 'Full Stack Developer.'],
+            typeSpeed: 80,
+            backSpeed: 80,
+            backDelay: 1000,
+        });
